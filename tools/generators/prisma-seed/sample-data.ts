195560fc61ef98c6c1df4892094d3b64e08c4260
Bram Borggreve <borggreve@gmail.com>
2021-01-17 18:05:51 -0500

    feat: add generator for api-features

--- tools/generators/prisma-seed/sample-data.ts
+++ tools/generators/prisma-seed/sample-data.ts
@@ -3,29 +3,10 @@ import { createSchema, createTenant, createUser } from './lib/helpers'
 
 export const users: UserCreateInput[] = [
   createUser('superadmin', 'superadmin', 'superadmin@example.com', Role.SuperAdmin),
-  createUser('admin', 'admin', 'admin@example.com', Role.Admin),
   createUser('user', 'user', 'user@example.com', Role.User),
 ]
 
 export const tenants: TenantCreateInput[] = [
-  createTenant(
-    `Tenant for SuperAdmin`,
-    [
-      createSchema('Tenant for SuperAdmin, schema a'),
-      createSchema('Tenant for SuperAdmin, schema b'),
-      createSchema('Tenant for SuperAdmin, schema c'),
-    ],
-    ['superadmin'],
-  ),
-  createTenant(
-    `Tenant for Admin`,
-    [
-      createSchema('Tenant for Admin, schema a'),
-      createSchema('Tenant for Admin, schema b'),
-      createSchema('Tenant for Admin, schema c'),
-    ],
-    ['admin'],
-  ),
   createTenant(
     `Tenant for User`,
     [
