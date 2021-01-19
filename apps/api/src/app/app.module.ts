fe6a215e8434647e7a93720f655b5be9dc3b93c8
Bram Borggreve <borggreve@gmail.com>
2021-01-19 15:00:49 -0500

    feat: make API serve frontend

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,7 +3,12 @@ import { ApiCoreFeatureModule } from '@metadata/api/core/feature'
 import { ApiSchemaFeatureModule } from '@metadata/api/schema/feature'
 import { ApiTenantFeatureModule } from '@metadata/api/tenant/feature'
 import { ApiUserFeatureModule } from '@metadata/api/user/feature'
-import { Module } from '@nestjs/common'
+import { Logger, Module } from '@nestjs/common'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { ensureDirSync, existsSync, writeFileSync } from 'fs-extra'
+import { join } from 'path'
+
+const rootPath = join(__dirname, '..', 'web')
 
 @Module({
   imports: [
@@ -12,6 +17,18 @@ import { Module } from '@nestjs/common'
     ApiSchemaFeatureModule,
     ApiTenantFeatureModule,
     ApiUserFeatureModule,
+    ServeStaticModule.forRoot({
+      rootPath,
+      exclude: ['/api/*', '/graphql'],
+    }),
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    if (!existsSync(rootPath)) {
+      ensureDirSync(rootPath)
+      writeFileSync(join(rootPath, 'index.html'), `<pre>Run 'yarn build:web' to build the frontend.</pre>`)
+      Logger.verbose(`Created static root path ${rootPath}`)
+    }
+  }
+}
