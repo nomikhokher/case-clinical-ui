f79cdd97ef6f8e71019a097824d5944fafd05ece
nomikhokher <nomankhokher786@gmail.com>
2021-04-01 01:43:04 +0500

    modify; sidebar/navigation

--- libs/web/ui/sidebar-page/src/lib/web-ui-sidebar-page.component.ts
+++ libs/web/ui/sidebar-page/src/lib/web-ui-sidebar-page.component.ts
@@ -13,15 +13,18 @@ export interface WebUiSidebarPageHeader {
   selector: 'ui-sidebar-page',
   template: `
     <ui-page [headerTitle]="headerTitle">
-      <div class="lg:grid lg:grid-cols-12 lg:gap-x-5">
-        <aside class="py-3 md:py-6 md:px-2 lg:py-0 lg:px-0 lg:col-span-3">
+      <div class="lg:grid lg:grid-cols-12 lg:gap-x-5 h-screen">
+        <aside class="py-3 md:py-6 md:px-2 lg:py-0 lg:px-0 lg:col-span-3 h-screen flex">
+        <div class="flex-1 flex overflow-hidden">
+        <!-- Scrollable container -->
+        <div class="flex-1 overflow-y-scroll">
           <nav class="space-y-1 md:space-y-3">
             <ng-container *ngFor="let link of links">
               <ng-container *ngIf="!link.path">
                 <div
                   class="uppercase dark:border-gray-800 dark:text-gray-500 text-gray-900 rounded-md px-5 pb-0 py-2 flex items-center text-sm tracking-wider"
                 >
-                  {{ link.label }}
+                {{ link.label }}
                 </div>
               </ng-container>
               <ng-container *ngIf="link.path">
@@ -38,9 +41,12 @@ export interface WebUiSidebarPageHeader {
               </ng-container>
             </ng-container>
           </nav>
+          </div>
+          </div>
         </aside>
 
-        <div class="md:px-2 lg:px-0 lg:col-span-9">
+        
+        <div class="md:px-2 lg:px-0 lg:col-span-9 overflow-y-scroll">
           <ng-content></ng-content>
         </div>
       </div>
