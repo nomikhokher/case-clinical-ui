af4a34aa950620a0c53c5883f493c2a583e2a88b
Nouman Khokher <nomankhokher786@gmail.com>
2021-06-04 00:31:57 +0500

    [calendar:fix]

--- libs/web/dev/feature/src/lib/dev-calendar/dev-calendar.component.ts
+++ libs/web/dev/feature/src/lib/dev-calendar/dev-calendar.component.ts
@@ -4,15 +4,7 @@ import { DevCalendarStore } from './dev-calendar.store'
 @Component({
   template: `
     <ng-container *ngIf="vm$ | async as vm">
-      <div class="p-4 shadow rounded-lg bg-gray-100 dark:bg-gray-800">
-        <pre class="text-xs dark:text-gray-500">{{ vm.items | json }}</pre>
-      </div>
-      <code class="text-xs px-2 py-1 dark:bg-gray-800 rounded-md opacity-70">
-        Component: libs/web/dev/feature/src/lib/dev-calendar/dev-calendar.component.ts
-      </code>
-      <ui-preview>
-        <ui-calendar></ui-calendar>
-      </ui-preview>
+      <ui-calendar></ui-calendar>
     </ng-container>
   `,
   providers: [DevCalendarStore],
