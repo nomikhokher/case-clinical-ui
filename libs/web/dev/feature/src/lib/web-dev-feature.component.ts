cb4f2896613c70db80739056266320c5a0c343c0
Brian Rabil <17106005+BrianRabil@users.noreply.github.com>
2021-06-22 17:53:46 -0400

    Layout components

--- libs/web/dev/feature/src/lib/web-dev-feature.component.ts
+++ libs/web/dev/feature/src/lib/web-dev-feature.component.ts
@@ -3,479 +3,294 @@ import { WebLayoutLink } from '@schema-driven/web/layout'
 
 @Component({
   template: `
-    <ui-main-page [headingTitle]="headingTitle" [headingLead]="headingLead" [links]="links">
-      <router-outlet></router-outlet>
-    </ui-main-page>
+    <div class="bg-gray-50 dark:bg-gray-900">
+      <ui-main-page [headingTitle]="headingTitle" [links]="links">
+        <router-outlet></router-outlet>
+      </ui-main-page>
+    </div>
   `,
 })
 export class WebDevFeatureComponent {
-  headingTitle = 'UI Components'
-  headingLead =
-    'Form layouts, tables, modal windows — everything you need to build beautiful responsive web applications.'
-
+  headingTitle = 'Components'
   links: WebLayoutLink[] = [
     {
-      label: 'Full Calendar',
+      title: 'Application UI',
+      subTitle: `Form layouts, tables, modal windows — everything you need to build beautiful responsive web applications.`,
       children: [
         {
-          label: 'Full-Calendar',
-          route: 'full-calendars',
-          image: '/assets/images/stacked.png',
+          label: 'Full Calendar',
           children: [
             {
-              label: 'Full Calendar',
+              label: 'Full-Calendar',
+              route: 'full-calendars',
+              image: '/assets/images/stacked.png',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Dashboard',
-      children: [
         {
           label: 'Dashboard',
-          route: 'dashboard',
-          image: '/assets/images/stacked.png',
           children: [
             {
-              label: 'Stacked Layouts',
+              label: 'Dashboard',
+              route: 'dashboard',
+              image: '/assets/images/stacked.png',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Headings',
-      children: [
         {
-          label: 'Page Headings',
-          route: 'page-headings',
-          image: '/assets/images/page-headings.png',
+          label: 'Headings',
           children: [
             {
               label: 'Page Headings',
+              route: 'page-headings',
+              image: '/assets/images/page-headings.png',
             },
-          ],
-        },
-        {
-          label: 'Card Headings',
-          route: 'card-headings',
-          image: '/assets/images/card-headings.png',
-          children: [
             {
               label: 'Card Headings',
+              route: 'card-headings',
+              image: '/assets/images/card-headings.png',
             },
-          ],
-        },
-        {
-          label: 'Section Headings',
-          route: 'section-headings',
-          image: '/assets/images/section-headings.png',
-          children: [
             {
               label: 'Section Headings',
+              route: 'section-headings',
+              image: '/assets/images/section-headings.png',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Data Display',
-      children: [
         {
-          label: 'Stats',
-          route: 'stats',
-          image: '/assets/images/stats.png',
+          label: 'Data Display',
           children: [
             {
               label: 'Stats',
+              route: 'stats',
+              image: '/assets/images/stats.png',
             },
-          ],
-        },
-        {
-          label: 'Description List',
-          route: 'description-list',
-          image: '/assets/images/description-lists.png',
-          children: [
             {
               label: 'Description List',
+              route: 'description-list',
+              image: '/assets/images/description-lists.png',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Lists',
-      children: [
         {
-          label: 'Tables',
-          route: 'table-lists',
-          image: '/assets/images/table-lists.png',
+          label: 'Lists',
           children: [
             {
               label: 'Tables',
+              route: 'table-lists',
+              image: '/assets/images/table-lists.png',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Forms',
-      children: [
         {
           label: 'Forms',
-          route: 'forms',
-          image: '/assets/images/form-layouts.png',
           children: [
             {
-              label: 'Form Layouts',
+              label: 'Forms',
+              route: 'forms',
+              image: '/assets/images/form-layouts.png',
             },
           ],
         },
-      ],
-    },
 
-    {
-      label: 'Layout',
-      children: [
         {
-          label: 'Containers',
-          route: 'containers',
-          image: '/assets/images/containers.png',
+          label: 'Layout',
           children: [
             {
               label: 'Containers',
+              route: 'containers',
+              image: '/assets/images/containers.png',
             },
-          ],
-        },
-        {
-          label: 'Working.......',
-          route: 'panels',
-          image: '/assets/images/panels.png',
-          children: [
             {
               label: 'Panels',
+              route: 'panels',
+              image: '/assets/images/panels.png',
             },
-          ],
-        },
-        {
-          label: 'List Containers',
-          route: 'list-containers',
-          image: '/assets/images/list-containers.png',
-          children: [
             {
-              label: 'List containers',
+              label: 'List Containers',
+              route: 'list-containers',
+              image: '/assets/images/list-containers.png',
             },
-          ],
-        },
-        {
-          label: 'Media Objects',
-          route: 'media-objects',
-          image: '/assets/images/media-objects.png',
-          children: [
             {
-              label: 'media-objects',
+              label: 'Media Objects',
+              route: 'media-objects',
+              image: '/assets/images/media-objects.png',
             },
-          ],
-        },
-        {
-          label: 'Dividers',
-          route: 'dividers',
-          image: '/assets/images/dividers.png',
-          children: [
             {
               label: 'Dividers',
+              route: 'dividers',
+              image: '/assets/images/dividers.png',
             },
           ],
         },
-      ],
-    },
 
-    {
-      label: 'Content Layout',
-      children: [
         {
-          label: 'Data Table',
-          route: 'table',
-          image: '/assets/images/tables.png',
+          label: 'Content Layout',
           children: [
             {
-              label: 'Data Table Layout',
+              label: 'Data Table',
+              route: 'table',
+              image: '/assets/images/tables.png',
             },
           ],
         },
-      ],
-    },
 
-    {
-      label: 'Calendars',
-      children: [
         {
-          label: 'Calendars Ui',
-          route: 'calendars',
-          image: '/assets/images/tables.png',
+          label: 'Calendars',
           children: [
             {
-              label: 'Calendar',
+              label: 'Calendars Ui',
+              route: 'calendars',
+              image: '/assets/images/tables.png',
+            },
+            {
+              label: 'Mini Calendars Ui',
+              route: 'mini-calendars',
+              image: '/assets/images/tables.png',
             },
           ],
         },
+
         {
-          label: 'Mini Calendars Ui',
-          route: 'mini-calendars',
-          image: '/assets/images/tables.png',
+          label: 'Notification',
           children: [
             {
-              label: 'Mini Calendar',
+              label: 'Toasts',
+              route: 'toasts',
+              image: '/assets/images/notifications.png',
             },
           ],
         },
-      ],
-    },
 
-    {
-      label: 'Notification',
-      children: [
         {
-          label: 'Toasts',
-          route: 'toasts',
-          image: '/assets/images/notifications.png',
+          label: 'Feedback',
           children: [
+            {
+              label: 'Alert',
+              route: 'alerts',
+              image: '/assets/images/alerts.png',
+            },
+          ],
+        },
+        {
+          label: 'Overlays',
+          children: [
+            {
+              label: 'Modal',
+              route: 'modals',
+              image: '/assets/images/modals.png',
+            },
+
             {
               label: 'Notifications',
+              route: 'notifications',
+              image: '/assets/images/notifications.png',
             },
-          ],
-        },
-      ],
-    },
-
-    {
-      label: 'Feedback',
-      children: [
-        {
-          label: 'Alert',
-          route: 'alerts',
-          image: '/assets/images/alerts.png',
-          children: [
-            {
-              label: 'Alerts',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      label: 'Overlays',
-      children: [
-        {
-          label: 'Modal',
-          route: 'modals',
-          image: '/assets/images/modals.png',
-          children: [
-            {
-              label: 'Modals',
-            },
-          ],
-        },
-
-        {
-          label: 'Notifications',
-          route: 'notifications',
-          image: '/assets/images/notifications.png',
-          children: [
-            {
-              label: 'Notifications',
-            },
-          ],
-        },
-        {
-          label: 'Slide-overs',
-          route: 'slideovers',
-          image: '/assets/images/notifications.png',
-          children: [
             {
               label: 'Slide-overs',
+              route: 'slideovers',
+              image: '/assets/images/notifications.png',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Elements',
-      children: [
         {
-          label: 'Avatars',
-          route: 'avatars',
-          image: '/assets/images/avatars.png',
+          label: 'Elements',
           children: [
             {
               label: 'Avatars',
+              route: 'avatars',
+              image: '/assets/images/avatars.png',
             },
-          ],
-        },
-        {
-          label: 'Dropdown',
-          route: 'dropdown',
-          image: '/assets/images/dropdowns.png',
-          children: [
             {
-              label: 'Dropdowns',
+              label: 'Dropdown',
+              route: 'dropdown',
+              image: '/assets/images/dropdowns.png',
             },
-          ],
-        },
-        {
-          label: 'badge',
-          route: 'badges',
-          image: '/assets/images/badges.png',
-          children: [
             {
-              label: 'Badges',
+              label: 'badge',
+              route: 'badges',
+              image: '/assets/images/badges.png',
             },
-          ],
-        },
-        {
-          label: 'Buttons',
-          route: 'buttons',
-          image: '/assets/images/buttons.png',
-          children: [
             {
               label: 'Buttons',
+              route: 'buttons',
+              image: '/assets/images/buttons.png',
             },
-          ],
-        },
-        {
-          label: 'Group Buttons',
-          route: 'group-button',
-          image: '/assets/images/button-groups.png',
-          children: [
             {
-              label: 'Button Groups',
+              label: 'Group Buttons',
+              route: 'group-button',
+              image: '/assets/images/button-groups.png',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Navigation',
-      children: [
         {
-          label: 'Tree',
-          route: 'tree',
-          image: '/assets/images/vertical-navigation.png',
+          label: 'Navigation',
           children: [
             {
               label: 'Tree',
+              route: 'tree',
+              image: '/assets/images/vertical-navigation.png',
             },
-          ],
-        },
-        {
-          label: 'Steps',
-          route: 'steps',
-          image: '/assets/images/steps.png',
-          children: [
             {
               label: 'Steps',
+              route: 'steps',
+              image: '/assets/images/steps.png',
             },
-          ],
-        },
-        {
-          label: 'Breadcrumbs',
-          route: 'breadcrumbs',
-          image: '/assets/images/breadcrumbs.png',
-          children: [
             {
               label: 'Breadcrumbs',
+              route: 'breadcrumbs',
+              image: '/assets/images/breadcrumbs.png',
             },
-          ],
-        },
-        {
-          label: 'Tabs',
-          route: 'tabs',
-          image: '/assets/images/tabs.png',
-          children: [
             {
               label: 'Tabs',
+              route: 'tabs',
+              image: '/assets/images/tabs.png',
             },
-          ],
-        },
-        {
-          label: 'Pagination',
-          route: 'paginations',
-          image: '/assets/images/pagination.png',
-          children: [
             {
               label: 'Pagination',
+              route: 'paginations',
+              image: '/assets/images/pagination.png',
             },
-          ],
-        },
-        {
-          label: 'Vertical Navigation',
-          route: 'vertical-nav',
-          image: '/assets/images/vertical-navigation.png',
-          children: [
             {
               label: 'Vertical Navigation',
+              route: 'vertical-nav',
+              image: '/assets/images/vertical-navigation.png',
             },
-          ],
-        },
-        {
-          label: 'Navbars',
-          route: 'navbars',
-          image: '/assets/images/navbars.png',
-          children: [
             {
               label: 'Navbars',
+              route: 'navbars',
+              image: '/assets/images/navbars.png',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Syntax Highlight',
-      children: [
         {
-          label: 'JSON Formatter',
-          route: 'json',
-          image:
-            'https://storage.googleapis.com/website-dev-images/sites/default/files/shutterstock_671233504%20%281%29.jpg',
+          label: 'Syntax Highlight',
           children: [
             {
-              label: 'Json Api',
+              label: 'JSON Formatter',
+              route: 'json',
+              image:
+                'https://storage.googleapis.com/website-dev-images/sites/default/files/shutterstock_671233504%20%281%29.jpg',
             },
           ],
         },
-      ],
-    },
-    {
-      label: 'Lists',
-      children: [
         {
-          label: 'Feeds',
-          route: 'feeds',
-          image: '/assets/images/feeds.png',
+          label: 'Lists',
           children: [
             {
               label: 'Feeds',
+              route: 'feeds',
+              image: '/assets/images/feeds.png',
             },
-          ],
-        },
-        {
-          label: 'Stacked List',
-          route: 'lists',
-          image: '/assets/images/stacked-lists.png',
-          children: [
             {
-              label: 'Stacked Lists',
+              label: 'Stacked List',
+              route: 'lists',
+              image: '/assets/images/stacked-lists.png',
             },
-          ],
-        },
-        {
-          label: 'Working....',
-          route: 'grid-lists',
-          image: '/assets/images/grid-lists.png',
-          children: [
             {
-              label: 'Grid Lists',
+              label: 'Working....',
+              route: 'grid-lists',
+              image: '/assets/images/grid-lists.png',
             },
           ],
         },
--- libs/web/ui/page-header/src/lib/web-ui-page-header.component.ts
+++ libs/web/ui/page-header/src/lib/web-ui-page-header.component.ts
@@ -5,7 +5,7 @@ import { Crumb } from '@schema-driven/web/ui/breadcrumbs'
 @Component({
   selector: 'ui-page-header',
   template: `
-    <div class="lg:flex lg:items-center lg:justify-between mx-auto">
+    <div class="lg:flex lg:items-center lg:justify-between mx-auto py-10">
       <div class="flex-1 min-w-0">
         <h2 class="text-2xl font-bold leading-7 text-gray-900 dark:text-gray-100 sm:text-3xl sm:truncate">
           {{ title }}
@@ -14,15 +14,15 @@ import { Crumb } from '@schema-driven/web/ui/breadcrumbs'
           <ng-container *ngFor="let entry of meta">
             <div class="mt-2 flex items-center text-sm text-gray-300 dark:text-gray-400 text-gray-500">
               <ui-icon
+                *ngIf="entry.icon"
                 class="flex-shrink-0 mr-1.5 h-5 w-5 dark:text-gray-500 text-gray-400"
-                [icon]="meta.icon"
+                [icon]="entry.icon"
               ></ui-icon>
-              {{ meta.label }}
+              {{ entry.label }}
             </div>
           </ng-container>
         </div>
       </div>
-
       <ng-container *ngIf="controlsTemplate">
         <div class="mt-5 flex lg:mt-0 lg:ml-4">
           <ng-container *ngTemplateOutlet="controlsTemplate"></ng-container>
