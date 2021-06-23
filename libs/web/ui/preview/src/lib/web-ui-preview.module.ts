dc6527aee045df41c9d43718dc24a89dcc3b1098
Noman Khokher <nomankhokher786@gmail.com>
2021-06-23 10:18:33 -0700

    fixing code preview

--- libs/web/ui/preview/src/lib/web-ui-preview.module.ts
+++ libs/web/ui/preview/src/lib/web-ui-preview.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { WebUiCodeModule } from '@schema-driven/web/ui/code'
 import { WebUiPreviewComponent } from './web-ui-preview.component'
+import { FormsModule } from '@angular/forms'
+import { ServiceCodepreview } from '../../../codepreview.service'
 
 @NgModule({
-  imports: [CommonModule, RouterModule, WebUiCodeModule],
+  imports: [CommonModule, RouterModule, WebUiCodeModule, FormsModule],
   declarations: [WebUiPreviewComponent],
   exports: [WebUiPreviewComponent],
+  providers: [ServiceCodepreview],
 })
 export class WebUiPreviewModule {}
