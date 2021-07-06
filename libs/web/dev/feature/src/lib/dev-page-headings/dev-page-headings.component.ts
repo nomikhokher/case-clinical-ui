d51d7fa8a69627298266af4a60462aeed8b2f206
Filmstar!2 <nomankhokher786@gmail.com>
2021-07-06 08:59:01 -0700

    improve page header component

--- libs/web/dev/feature/src/lib/dev-page-headings/dev-page-headings.component.ts
+++ libs/web/dev/feature/src/lib/dev-page-headings/dev-page-headings.component.ts
@@ -24,6 +24,9 @@ import { DevPageHeadingsStore } from './dev-page-headings.store'
             [title]="config.title"
             [meta]="config.meta"
             [controlsTemplate]="headerControls"
+            [imgSource]="
+              'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+            "
           ></ui-page-header>
         </div>
 
