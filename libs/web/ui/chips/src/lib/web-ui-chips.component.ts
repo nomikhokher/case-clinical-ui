e54185eb8bbc73902a8b6fec5b1231916ba03f79
Filmstar!2 <nomankhokher786@gmail.com>
2021-07-08 11:12:12 -0700

    pass all data from store.ts

--- libs/web/ui/chips/src/lib/web-ui-chips.component.ts
+++ libs/web/ui/chips/src/lib/web-ui-chips.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core'
-import { Chips } from '../../../../dev/feature/src/lib/dev-chips/dev-chips.component'
 
 @Component({
   selector: 'ui-chips',
@@ -8,12 +7,13 @@ import { Chips } from '../../../../dev/feature/src/lib/dev-chips/dev-chips.compo
       <div class="flex flex-wrap justify-center">
         <div
           *ngFor="let chip of chips; let i = index"
-          class="flex justify-center items-center m-1 font-medium py-1 px-2 rounded-full {{ chip.textColor }} {{
+          class="flex justify-center items-center m-1 font-medium py-1.5  rounded-full {{ chip.textColor }} {{
             chip.bgColor
           }} border hover:{{ chip.hoverColor }} cursor-pointer "
+          [ngClass]="chip.cross ? 'px-2' : 'px-4'"
         >
-          <div *ngIf="chip.icon">
-            <ui-icon icon="{{ chip.icon }}" class="feather feather-heart w-4 h-5 mr-1"></ui-icon>
+          <div *ngIf="chip.icon" class="flex flex-auto flex-row-reverse">
+            <ui-icon size="md" class="h-5 w-5 " icon="{{ chip.icon }}"></ui-icon>
           </div>
           <div
             slot="avatar"
@@ -22,15 +22,10 @@ import { Chips } from '../../../../dev/feature/src/lib/dev-chips/dev-chips.compo
           >
             <img class="rounded-full" alt="A" src="{{ chip.img }}" />
           </div>
-          <div class="text-xs font-normal leading-none max-w-full flex-initial">Hello!</div>
-          <div class="flex flex-auto flex-row-reverse">
-            <div *ngIf="chip.cross">
-              <ui-icon
-                icon="{{ chip.cross }}"
-                class="feather feather-x cursor-pointer hover:text-yellow-400 rounded-full w-4 h-5 mx-1"
-                (click)="deleteItem(i)"
-              ></ui-icon>
-            </div>
+          <div class="text-xs font-normal leading-none max-w-full flex-initial">{{ chip.text }}</div>
+
+          <div *ngIf="chip.cross" class="ml-2">
+            <ui-icon size="lg" class="h-3 w-3" icon="cross" (click)="deleteItem(i)"></ui-icon>
           </div>
         </div>
       </div>
@@ -38,7 +33,7 @@ import { Chips } from '../../../../dev/feature/src/lib/dev-chips/dev-chips.compo
   `,
 })
 export class WebUiChipsComponent {
-  @Input() chips: Chips[]
+  @Input() chips?: Array<any>
 
   deleteItem(i) {
     console.log(i)
