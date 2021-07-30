890ec8ef3248b5efa8579cee57691926b8055f46
nomikhoker <nomankhokher786@gmail.com>
2021-07-30 19:13:54 +0500

    imporve store and ui of card heading

--- libs/web/dev/feature/src/lib/dev-card-heading/dev-card-heading.store.ts
+++ libs/web/dev/feature/src/lib/dev-card-heading/dev-card-heading.store.ts
@@ -78,6 +78,7 @@ export class DevCardHeadingStore extends ComponentStore<DevCardHeadingState> {
             { title: ['Ton Cook', 'Judith Black', 'Joseph Rodriguez'] },
             {
               image: [
+                '',
                 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
                 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
                 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
--- libs/web/ui/card-heading/src/lib/web-ui-card-heading.component.ts
+++ libs/web/ui/card-heading/src/lib/web-ui-card-heading.component.ts
@@ -14,9 +14,12 @@ import { Component, Input } from '@angular/core'
                 <img class="h-12 w-12 rounded-full" src="{{ profile.image }}" alt="" />
               </span>
             </div>
-            <ng-conatiner *ngIf="!profile.image">
-              <ui-icon size="lg" icon="{{ profile.icon }}" class="h-6 w-6 text-gray-500 dark:text-gray-200"></ui-icon>
-            </ng-conatiner>
+            <ui-icon
+              *ngIf="!profile.image ? true : false"
+              size="lg"
+              icon="{{ profile.icon }}"
+              class="h-6 w-6 text-gray-500 dark:text-gray-200"
+            ></ui-icon>
             <div class="ml-4">
               <h3 class="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100">
                 {{ profile.title }}
