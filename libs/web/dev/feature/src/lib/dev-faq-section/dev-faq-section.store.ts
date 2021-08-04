97176d9ae5321281d10c939699dda54bdd81d2fe
Noman Khokher <nomankhokher786@gmail.com>
2021-08-04 11:58:25 -0700

    add play-ground section of faq-section-component

--- libs/web/dev/feature/src/lib/dev-faq-section/dev-faq-section.store.ts
+++ libs/web/dev/feature/src/lib/dev-faq-section/dev-faq-section.store.ts
@@ -62,6 +62,7 @@ const config: Configs = {
       prop: '[faqStyle]',
       description: 'Change the style of FAQ Section.',
       dataType: 'String',
+      type: ['sideByside', 'show', 'hidden'],
     },
     {
       label: 'Background Color',
@@ -69,7 +70,39 @@ const config: Configs = {
       description: 'Adjust the background color.',
       dataType: 'String',
     },
-    { label: 'Content', prop: '[content]', description: 'Show the content in section.', dataType: 'Object' },
+    {
+      label: 'Content',
+      prop: '[content]',
+      description: 'Show the content in section.',
+      dataType: 'Object',
+      typeArray: [
+        [
+          { question: 'How do you make holy water?' },
+          {
+            answer:
+              'You boil the hell out of it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat',
+          },
+        ],
+        [
+          { question: `What's the best thing about Switzerland?` },
+          {
+            answer: `I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat`,
+          },
+        ],
+        [
+          { question: `What do you call someone with no body and no nose?` },
+          {
+            answer: `Nobody knows. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.`,
+          },
+        ],
+        [
+          { question: `Why do you never see elephants hiding in trees?` },
+          {
+            answer: `Because they're so good at it. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.`,
+          },
+        ],
+      ],
+    },
   ],
 }
 
