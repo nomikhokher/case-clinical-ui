16473b71a1d260c059320be7066e62cc8189d307
Noman Khokher <nomankhokher786@gmail.com>
2021-09-02 06:58:51 -0700

    send dynamic data to Preview Section

--- libs/web/dev/feature/src/lib/dev-list-container/dev-list-container.component.ts
+++ libs/web/dev/feature/src/lib/dev-list-container/dev-list-container.component.ts
@@ -6,25 +6,17 @@ import { DevListContainerStore } from './dev-list-container.store'
     <ng-container *ngIf="vm$ | async as vm">
       <ui-preview
         [code]="codePreview[0]"
-        [title]="vm.config.headerTitle"
-        [githubURL]="vm.config.githubURL"
-        [directory]="vm.config.directory"
-        [breadcrumbs]="vm.config.breadcrumbs"
+        [githubURL]="vm.config.items.githubURL"
+        [title]="vm.config.items.headerTitle"
+        [directory]="vm.config.items.directory"
         [component_inputs]="vm.config.component_inputs"
+        [codeObj]="vm.config.items"
       >
-        <ui-list-container [classNames]="vm.config.items.classNames" [roundedDividers]="">
-          <ng-container class="roundedDividers">
-            <p class="py-4">Simple with dividers</p>
-            <p class="py-4">Simple with dividers</p>
-          </ng-container>
-        </ui-list-container>
-      </ui-preview>
-      <ui-preview [code]="codePreview[1]">
         <ui-list-container
           [classNames]="vm.config.items.classNames"
           [roundedDividers]="vm.config.items.roundedDividers"
         >
-          <ng-container class="roundedDividersNot">
+          <ng-container class="content">
             <p class="py-4">Simple with dividers</p>
             <p class="py-4">Simple with dividers</p>
           </ng-container>
@@ -38,18 +30,22 @@ export class DevListContainerComponent {
   readonly vm$ = this.store.vm$
 
   constructor(private readonly store: DevListContainerStore) {}
-  public codePreview = [
-    `import { WebUiListContainerModule } from '@schema-driven/web/ui/list-container'\n\n<ui-list-container [classNames]="''" [roundedDividers]="">
-    <ng-container class="roundedDividers">
-      <p class="py-4">Simple with dividers</p>
-      <p class="py-4">Simple with dividers</p>
-    </ng-container>
-  </ui-list-container>`,
-    `import { WebUiListContainerModule } from '@schema-driven/web/ui/list-container'\n\n<ui-list-container [classNames]="''" [roundedDividers]="roundedDividers">
-    <ng-container class="roundedDividersNot">
-      <p class="py-4">Simple with dividers</p>
-      <p class="py-4">Simple with dividers</p>
-    </ng-container>
-  </ui-list-container> \n\n roundedDividers = true`,
-  ]
+  public codePreview: Array<any>
+  ngOnInit(): void {
+    this.vm$.subscribe((result) => {
+      this.codePreview = [
+        `import { WebUiListContainerModule } from '@schema-driven/web/ui/list-container'
+        \n\n<ui-list-container [classNames]="''" [roundedDividers]="roundedDividers">
+            <ng-container class="content">
+              <p class="py-4">Simple with dividers</p>
+              <p class="py-4">Simple with dividers</p>
+            </ng-container>
+        </ui-list-container>
+        \n
+        classNames = ${result.config.items.classNames},
+        roundedDividers = ${result.config.items.roundedDividers},
+        `,
+      ]
+    })
+  }
 }
--- libs/web/dev/feature/src/lib/dev-media-object/dev-media-object.component.ts
+++ libs/web/dev/feature/src/lib/dev-media-object/dev-media-object.component.ts
@@ -37,7 +37,7 @@ import { DevMediaObjectStore } from './dev-media-object.store'
           </ng-container>
         </ui-media-object>
       </ui-preview>
-      <ui-preview [code]="codePreview[1]">
+      <ui-preview [code]="codePreview[0]">
         <!-- you add only height '1,2,3,4,5,6,7,8,9,10,11,12,14,16' if use full height image then you not use vertical direction or 'full', by default set value is '16' -->
         <!-- you add only width '1,2,3,4,5,6,7,8,9,10,11,12,14,16' and not use 'full'value, by default set value is '16' -->
         <ui-media-object icon="imageAvatar" [circle]="circle" height="full" width="" horizontalDirection="right">
@@ -51,7 +51,7 @@ import { DevMediaObjectStore } from './dev-media-object.store'
         </ui-media-object>
       </ui-preview>
 
-      <ui-preview [code]="codePreview[2]">
+      <ui-preview [code]="codePreview[0]">
         <ui-media-object [data]="data" [circle]="circle" height="" width="" horizontalDirection="rights">
           <ui-media-object
             class="objectData2"
@@ -91,37 +91,43 @@ export class DevMediaObjectComponent {
 
   constructor(private readonly store: DevMediaObjectStore) {}
 
-  public codePreview = [
-    ` import { WebUiMediaObjectModule } from '@schema-driven/web/ui/media-object\n\n<ui-media-object icon="imageAvatar" [circle]="circle" verticalDirection="center" horizontalDirection="left">
-    <ng-container class="objectData">
-      <h4 class="text-lg font-bold">Lorem ipsum</h4>
-      <p class="mt-1">
-        Repudiandae sint consequuntur vel. Amet ut nobis explicabo numquam expedita quia omnis voluptatem. Minus
-        quidem ipsam quia iusto.
-      </p>
-    </ng-container>
-  </ui-media-object> \n\n circle= false`,
-    `import { WebUiMediaObjectModule } from '@schema-driven/web/ui/media-object\n\n<ui-media-object icon="imageAvatar" [circle]="circle" height="full" width="" horizontalDirection="right">
-    <ng-container class="objectDataFullImage">
-      <h4 class="text-lg font-bold">Lorem ipsum</h4>
-      <p class="mt-1">
-        Repudiandae sint consequuntur vel. Amet ut nobis explicabo numquam expedita quia omnis voluptatem. Minus
-        quidem ipsam quia iusto.
-      </p>
-    </ng-container>
-  </ui-media-object> \n\n circle= false`,
-    `import { WebUiMediaObjectModule } from '@schema-driven/web/ui/media-object\n\n<ui-media-object [data]="data" [circle]="circle" height="" width="" horizontalDirection="rights">
-    <ui-media-object
-      class="objectData2"
-      *ngFor="let childData of data1"
-      [data]="childData"
-      [circle]="circle"
-      height=""
-      width=""
-      horizontalDirection="left"
-    >
-    </ui-media-object>\n\ncircle= false`,
-  ]
+  public codePreview: Array<any>
+  ngOnInit(): void {
+    this.vm$.subscribe((result) => {
+      this.codePreview = [
+        `import { WebUiMediaObjectModule } from '@schema-driven/web/ui/media-object' \n\n 
+        <ui-media-object
+        [circle]="vm.config.items.circle"
+        [verticalDirection]="vm.config.items.verticalDirection"
+        [height]="vm.config.items.height"
+        [width]="vm.config.items.width"
+        [horizontalDirection]="vm.config.items.horizontalDirection"
+      >
+        <ng-container class="objectData">
+          <h4 class="text-lg font-bold dark:text-gray-100">{{ vm.config.items.title }}</h4>
+          <p class="mt-1 dark:text-gray-300">
+            {{ vm.config.items.description }}
+          </p>
+        </ng-container>
+        <ng-container class="objectDataFullImage">
+          <h4 class="text-lg font-bold dark:text-gray-100">{{ vm.config.items.title }}</h4>
+          <p class="mt-1 dark:text-gray-300">
+            {{ vm.config.items.description }}
+          </p>
+        </ng-container>
+      </ui-media-object> \n\n
+
+
+      
+        direction = ${JSON.stringify(result.config.items.circle, null, '\t')}\n
+        horizontalDirection = ${JSON.stringify(result.config.items.horizontalDirection, null, '\t')}\n
+        verticalDirection = ${JSON.stringify(result.config.items.verticalDirection, null, '\t')}\n
+        height = ${JSON.stringify(result.config.items.height, null, '\t')}\n
+        width = ${JSON.stringify(result.config.items.width, null, '\t')}\n
+        `,
+      ]
+    })
+  }
 
   public data = {
     img: 'imageAvatar',
