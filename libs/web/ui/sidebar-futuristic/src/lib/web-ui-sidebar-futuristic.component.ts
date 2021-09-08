3130f4a8d2def1af1a75b9f9e883bf1e3358f70d
Noman Khokher <nomankhokher786@gmail.com>
2021-09-08 12:43:32 -0700

    add functionality to search components in Search Bar of all layouts

--- libs/web/ui/sidebar-futuristic/src/lib/web-ui-sidebar-futuristic.component.ts
+++ libs/web/ui/sidebar-futuristic/src/lib/web-ui-sidebar-futuristic.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core'
+import { Component, HostListener, Input } from '@angular/core'
+import { Router } from '@angular/router'
 import { User } from '@schema-driven/web/core/data-access'
+import { ServiceCodepreview } from '../../../codepreview.service'
 
 @Component({
   selector: 'ui-sidebar-futuristic',
@@ -26,6 +28,102 @@ import { User } from '@schema-driven/web/core/data-access'
   ],
   template: `
     <!-- This example requires Tailwind CSS v2.0+ -->
+    <div>
+      <ng-container *ngIf="isActive">
+        <div
+          role="combobox"
+          aria-haspopup="listbox"
+          aria-expanded="true"
+          class="fixed z-50 pt-16 flex items-start justify-center inset-0 sm:pt-24"
+        >
+          <div
+            (click)="outsideClick()"
+            x-transition:enter="ease-out duration-200"
+            x-transition:enter-start="opacity-0"
+            x-transition:enter-end="opacity-100"
+            x-transition:leave="ease-in duration-150"
+            x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0"
+            class="fixed inset-0 bg-gray-800 bg-opacity-60 transition-opacity"
+          ></div>
+          <div
+            x-transition:enter="ease-out duration-200"
+            x-transition:enter-start="opacity-0 scale-95"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="ease-in duration-150"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-95"
+            class="relative transform transition-all max-w-lg w-full px-4"
+          >
+            <div class="bg-white rounded-lg shadow-md overflow-hidden">
+              <form
+                role="search"
+                novalidate=""
+                class="relative flex items-center pr-4 justify-center"
+                :class="search ? 'shadow-sm' : ''"
+              >
+                <input
+                  (input)="onSearch($event)"
+                  style="caret-color: #6b7280"
+                  class="flex-auto -mr-9 appearance-none bg-transparent pl-4 pr-12 py-4 text-gray-600 text-base sm:text-sm placeholder-gray-500 focus:outline-none w-full"
+                  placeholder="Find components..."
+                />
+                <svg
+                  width="20"
+                  height="20"
+                  fill="none"
+                  class="flex-none text-gray-400 pointer-events-none absolute right-7 top-4"
+                >
+                  <circle
+                    cx="8.5"
+                    cy="8.5"
+                    r="5.75"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></circle>
+                  <path
+                    d="M17.25 17.25L13 13"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </form>
+              <ng-container hidden="">
+                <ul
+                  class="text-sm font-medium overflow-y-auto border-t border-gray-200 divide-y divide-gray-200 rounded-b-lg max-h-80 "
+                >
+                  <li *ngFor="let item of componentList">
+                    <a
+                      (click)="redirectTo(item)"
+                      tabindex="-1"
+                      class="flex cursor-pointer text-gray-600 justify-between p-4 hover:bg-gray-100 hover:theme-color-500"
+                    >
+                      <span class="whitespace-nowrap">{{ item.label }}</span>
+                      <img class="w-1/2 h-auto" src="{{ item.image }}" alt="" />
+                    </a>
+                  </li>
+                </ul>
+              </ng-container>
+
+              <div
+                x-show="search &amp;&amp; results.length === 0"
+                class="bg-gray-50 p-16 text-center"
+                style="display: none;"
+              >
+                <h2 class="text-gray-900 font-semibold mb-2">No results found</h2>
+                <p class="text-sm">
+                  We can’t find anything with that term at the moment, try searching something else.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ng-container>
+    </div>
     <div class="h-screen flex overflow-hidden">
       <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
       <div class="fixed inset-0 flex z-40 md:hidden" role="dialog" aria-modal="true" *ngIf="mobileSideBar">
@@ -216,12 +314,27 @@ import { User } from '@schema-driven/web/core/data-access'
           <!-- Sidebar component, swap this element with another sidebar if you like -->
           <div class="flex flex-col h-0 flex-1">
             <div class="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto aside-scrollbar">
-              <div class="flex items-center flex-shrink-0 px-4">
+              <div class="flex items-center flex-shrink-0 px-4  justify-around">
                 <img
                   class="h-8 w-auto"
                   src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
                   alt="Workflow"
                 />
+                <svg
+                  (click)="isActive = true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 text-white cursor-pointer"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
               </div>
               <nav class="mt-5 flex-1 px-2 space-y-1">
                 <div class="mt-4">
@@ -385,12 +498,38 @@ import { User } from '@schema-driven/web/core/data-access'
   `,
 })
 export class WebUiSidebarFuturisticComponent {
+  constructor(public searchService: ServiceCodepreview, public router: Router) {}
   public showMenu = false
   public mobileSideBar: boolean = false
+  public componentList
 
   @Input() notificationsLink?: string
   @Input() user?: User
   @Input() links: { label: string; route: string }[] = []
   @Input() profileLinks: { label: string; route: string }[] = []
   @Input() logo: string
+  isActive: boolean = false
+  ngOnInit(): void {
+    this.searchService.searchedArray$.subscribe((res) => {
+      this.componentList = res
+    })
+  }
+
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(evt: KeyboardEvent) {
+    this.isActive = false
+  }
+  onSearch(e: any) {
+    console.log('runs')
+
+    this.searchService.searchBar$.next(e.target.value)
+  }
+  outsideClick() {
+    this.isActive = false
+    this.searchService.searchBar$.next('')
+    this.componentList = []
+  }
+  redirectTo(i) {
+    this.router.navigate([`dev/${i.route}`])
+    this.outsideClick()
+  }
 }
--- libs/web/ui/sidebar-thin/src/lib/web-ui-sidebar-thin.component.ts
+++ libs/web/ui/sidebar-thin/src/lib/web-ui-sidebar-thin.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core'
+import { Component, HostListener, Input } from '@angular/core'
+import { Router } from '@angular/router'
 import { User } from '@schema-driven/web/core/data-access'
+import { ServiceCodepreview } from '../../../codepreview.service'
 
 @Component({
   selector: 'ui-sidebar-thin',
@@ -25,6 +27,102 @@ import { User } from '@schema-driven/web/core/data-access'
     `,
   ],
   template: `
+    <div>
+      <ng-container *ngIf="isActive">
+        <div
+          role="combobox"
+          aria-haspopup="listbox"
+          aria-expanded="true"
+          class="fixed z-50 pt-16 flex items-start justify-center inset-0 sm:pt-24"
+        >
+          <div
+            (click)="outsideClick()"
+            x-transition:enter="ease-out duration-200"
+            x-transition:enter-start="opacity-0"
+            x-transition:enter-end="opacity-100"
+            x-transition:leave="ease-in duration-150"
+            x-transition:leave-start="opacity-100"
+            x-transition:leave-end="opacity-0"
+            class="fixed inset-0 bg-gray-800 bg-opacity-60 transition-opacity"
+          ></div>
+          <div
+            x-transition:enter="ease-out duration-200"
+            x-transition:enter-start="opacity-0 scale-95"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="ease-in duration-150"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-95"
+            class="relative transform transition-all max-w-lg w-full px-4"
+          >
+            <div class="bg-white rounded-lg shadow-md overflow-hidden">
+              <form
+                role="search"
+                novalidate=""
+                class="relative flex items-center pr-4 justify-center"
+                :class="search ? 'shadow-sm' : ''"
+              >
+                <input
+                  (input)="onSearch($event)"
+                  style="caret-color: #6b7280"
+                  class="flex-auto -mr-9 appearance-none bg-transparent pl-4 pr-12 py-4 text-gray-600 text-base sm:text-sm placeholder-gray-500 focus:outline-none w-full"
+                  placeholder="Find components..."
+                />
+                <svg
+                  width="20"
+                  height="20"
+                  fill="none"
+                  class="flex-none text-gray-400 pointer-events-none absolute right-7 top-4"
+                >
+                  <circle
+                    cx="8.5"
+                    cy="8.5"
+                    r="5.75"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></circle>
+                  <path
+                    d="M17.25 17.25L13 13"
+                    stroke="currentColor"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></path>
+                </svg>
+              </form>
+              <ng-container hidden="">
+                <ul
+                  class="text-sm font-medium overflow-y-auto border-t border-gray-200 divide-y divide-gray-200 rounded-b-lg max-h-80 "
+                >
+                  <li *ngFor="let item of componentList">
+                    <a
+                      (click)="redirectTo(item)"
+                      tabindex="-1"
+                      class="flex cursor-pointer text-gray-600 justify-between p-4 hover:bg-gray-100 hover:theme-color-500"
+                    >
+                      <span class="whitespace-nowrap">{{ item.label }}</span>
+                      <img class="w-1/2 h-auto" src="{{ item.image }}" alt="" />
+                    </a>
+                  </li>
+                </ul>
+              </ng-container>
+
+              <div
+                x-show="search &amp;&amp; results.length === 0"
+                class="bg-gray-50 p-16 text-center"
+                style="display: none;"
+              >
+                <h2 class="text-gray-900 font-semibold mb-2">No results found</h2>
+                <p class="text-sm">
+                  We can’t find anything with that term at the moment, try searching something else.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ng-container>
+    </div>
     <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="h-screen flex bg-gray-50 overflow-hidden">
       <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
@@ -225,6 +323,27 @@ import { User } from '@schema-driven/web/core/data-access'
                   />
                 </div>
                 <nav aria-label="Sidebar" class="py-6 flex flex-col items-center space-y-3">
+                  <ng-container>
+                    <a
+                      class="flex items-center p-4 rounded-lg text-indigo-100 hover:theme-bg-300 hover:text-white cursor-pointer"
+                    >
+                      <svg
+                        (click)="isActive = true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6 text-white cursor-pointer"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                        />
+                      </svg>
+                    </a>
+                  </ng-container>
                   <ng-container *ngFor="let link of profileLinks; index as i">
                     <a
                       (click)="compactChildren(link.childs, i)"
@@ -394,6 +513,8 @@ import { User } from '@schema-driven/web/core/data-access'
   `,
 })
 export class WebUiSidebarThinComponent {
+  constructor(public searchService: ServiceCodepreview, public router: Router) {}
+
   public showMenu = false
   public asideWidth: number = 20
   public subChildren: any
@@ -408,6 +529,7 @@ export class WebUiSidebarThinComponent {
   @Input() links: { label: string; route: string }[] = []
   @Input() profileLinks: { label: string; route: string }[] = []
   @Input() logo: string
+  public componentList
 
   openMenu() {
     this.showMenu = !this.showMenu
@@ -431,5 +553,29 @@ export class WebUiSidebarThinComponent {
       this.compact.show = true
       this.asideWidth = 20
     }
+  }
+  isActive: boolean = false
+  ngOnInit(): void {
+    this.searchService.searchedArray$.subscribe((res) => {
+      this.componentList = res
+    })
+  }
+
+  @HostListener('document:keydown.escape', ['$event']) onKeydownHandler(evt: KeyboardEvent) {
+    this.isActive = false
+  }
+  onSearch(e: any) {
+    console.log('runs')
+
+    this.searchService.searchBar$.next(e.target.value)
+  }
+  outsideClick() {
+    this.isActive = false
+    this.searchService.searchBar$.next('')
+    this.componentList = []
+  }
+  redirectTo(i) {
+    this.router.navigate([`dev/${i.route}`])
+    this.outsideClick()
   }
 }
