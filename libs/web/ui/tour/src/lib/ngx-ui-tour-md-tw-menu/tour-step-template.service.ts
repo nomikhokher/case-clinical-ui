06eaf0d265800600566b28e647aeddd30857bcac
nomikhokher <nomankhokher786@gmail.com>
2021-12-04 01:03:23 +0500

    Add new component [dev:dev-tour && web:ui-tour].

---
+++ libs/web/ui/tour/src/lib/ngx-ui-tour-md-tw-menu/tour-step-template.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core'
+import { TourStepTemplateComponent } from './tour-step-template/tour-step-template.component'
+
+@Injectable()
+export class TourStepTemplateService {
+  public templateComponent!: TourStepTemplateComponent
+}
