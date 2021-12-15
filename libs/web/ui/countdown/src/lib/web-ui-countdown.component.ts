7cb1c36ec3196cafd5a9a549718ca6a5e4482904
nomikhokher <nomankhokhar786@gmail.com>
2021-12-15 18:27:10 +0500

    New updated countdown component.

--- libs/web/ui/countdown/src/lib/web-ui-countdown.component.ts
+++ libs/web/ui/countdown/src/lib/web-ui-countdown.component.ts
@@ -6,7 +6,9 @@ import { Component, Input, OnInit } from '@angular/core'
     <div class="dark:bg-gray-800 border dark:border-indigo-700 px-6 py-4 mb-3 md:mb-6 rounded-lg shadow">
       <div>
         <span class="font-mono text-6xl countdown">
-          <span style="--value:60;">{{ countDown }}<span style="font-size: 16px;">sec</span></span>
+          <span style="--value:60;" class="dark:text-white"
+            >{{ countDown }}<span style="font-size: 16px;">sec</span></span
+          >
         </span>
         <!-- <code>ui-countdown</code> -->
       </div>
