78ff26da2ad437727297c6d0592bbb94a5d70301
nomikhokher <nomankhokher786@gmail.com>
2022-02-08 18:48:26 +0500

    file input props and design

--- libs/web/ui/file-input/src/lib/web-ui-file-input.component.ts
+++ libs/web/ui/file-input/src/lib/web-ui-file-input.component.ts
@@ -3,53 +3,15 @@ import { Component, Input } from '@angular/core'
 @Component({
   selector: 'ui-file-input',
   template: `
-    <!-- <div class="flex justify-center">
-      <div class="mb-3 w-96">
-        <label for="formFile" class="form-label inline-block mb-2 text-gray-700">File Input Example</label>
-        <input
-          class="form-control
-                  block
-                  w-full
-                  px-3
-                  py-1.5
-                  text-base
-                  font-normal
-                  text-gray-700
-                  bg-{{ this.background }} bg-{{ this.background }}
-                  border border-solid border-gray-300
-                  rounded
-                  transition
-                  ease-in-out
-                  m-0
-                  focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-          type="file"
-          id="formFile"
-          accept="image/png, image/gif, image/jpeg"
-        />
-      </div>
-    </div> -->
     <div class="w-3/12 mx-auto">
-      <H1 class="p-4 text-center text-2xl dark:text-white">Upload your files</H1>
+      <h1 class="p-4 text-center text-2xl dark:text-white">Upload your files</h1>
       <div
+        *ngFor="let item of icon"
         class="relative rounded-lg h-36 rounded-lg themeBorder  flex justify-center items-center bg-white dark:bg-gray-800 border-dashed border-2 border-indigo-600 dark:border-white"
       >
         <div class="absolute">
           <div class="flex flex-col items-center">
-            <ui-icon icon="folderOpen"></ui-icon>
-            <!-- <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-14 mb-4 w-14 text-blue-600 dark:text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 19a2 2 0 01-2-2V7a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1M5 19h14a2 2 0 002-2v-5a2 2 0 00-2-2H9a2 2 0 00-2 2v5a2 2 0 01-2 2z"
-              />
-            </svg> -->
+            <ui-icon icon="{{ item.tempIcon }}"></ui-icon>
             <span class="block text-gray-500 text-sm font-normal dark:text-white">Drop files here to upload</span>
           </div>
         </div>
@@ -64,5 +26,12 @@ import { Component, Input } from '@angular/core'
   `,
 })
 export class WebUiFileInputComponent {
-  @Input() background?: string
+  @Input() icon?: Icon[]
+
+  ngOnInit() {
+    console.log(this.icon)
+  }
+}
+export interface Icon {
+  tempIcon?: string
 }
--- libs/web/ui/side-nav-bar/src/lib/web-ui-side-nav-bar.component.ts
+++ libs/web/ui/side-nav-bar/src/lib/web-ui-side-nav-bar.component.ts
@@ -3,20 +3,20 @@ import { Component, Input } from '@angular/core'
 @Component({
   selector: 'ui-side-nav-bar',
   template: `
-    <div class="h-full p-3 space-y-2 w-60 text-gray-800 ">
+    <div class="h-full p-3 space-y-2 w-60 text-gray-800 dark:text-white dark:border-white ">
       <div class="bg-{{ this.background }} border-solid border-2 border-gray-300 ">
         <div class="flex items-center p-2 space-x-4">
           <img src="https://source.unsplash.com/100x100/?portrait" alt="" class="w-12 h-12 rounded-full" />
           <div>
             <h2 class="text-lg font-semibold">{{ this.heading }}</h2>
             <span class="flex items-center space-x-1">
-              <a href="#" class="text-xs hover:underline text-gray-600">View profile</a>
+              <a href="#" class="text-xs hover:underline text-gray-600 dark:text-white">View profile</a>
             </span>
           </div>
         </div>
         <div class="divide-y divide-gray-300">
           <ul class="pt-2 pb-4 space-y-1 text-sm">
-            <li class="text-gray-900  " *ngFor="let item of menuItems">
+            <li class="text-gray-900 dark:text-white " *ngFor="let item of menuItems">
               <hr />
               <button class="flex items-center p-2 space-x-3 rounded-md w-full focus:ring-gray-500">
                 <ui-icon size="lg" class="h-6 w-6" icon="{{ item.icon }}"></ui-icon>
