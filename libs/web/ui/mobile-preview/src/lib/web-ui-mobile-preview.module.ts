e8dc1ef4f5293e2ff26e0fbe18cc90e2f234b47e
nomikhokher <nomikhokher@gmail.com>
2022-02-15 19:21:44 +0500

    {light-theme-componen.scss} new file & set design

--- libs/web/ui/mobile-preview/src/lib/web-ui-mobile-preview.module.ts
+++ libs/web/ui/mobile-preview/src/lib/web-ui-mobile-preview.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-import { WebUiCodeModule } from '@schema-driven/web/ui/code'
+import { WebUiMobileCodeModule } from '@schema-driven/web/ui/mobile-code'
 import { WebUiMobilePreviewComponent } from './web-ui-mobile-preview.component'
 import { WebUiPageModule } from '@schema-driven/web/ui/page'
 import { WebUiIconModule } from '@schema-driven/web/ui/icon'
@@ -18,7 +18,7 @@ import { WebUiMobileMutatorModule } from '@schema-driven/web/ui/mobile-mutator'
   imports: [
     CommonModule,
     RouterModule,
-    WebUiCodeModule,
+    WebUiMobileCodeModule,
     WebUiPageModule,
     WebUiIconModule,
     WebUiButtonModule,
