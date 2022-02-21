04598b3b939c376a135c8ab075aa6d58a89f18b3
nomikhokher <nomankhokher786@gmail.com>
2022-02-21 21:00:54 +0500

    { ui-mobile-camera } | design

--- libs/web/ui/mobile-camera/src/lib/web-ui-mobile-camera.component.ts
+++ libs/web/ui/mobile-camera/src/lib/web-ui-mobile-camera.component.ts
@@ -3,11 +3,42 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'ui-mobile-camera',
   template: `
-    <div class="dark:bg-gray-800 border dark:border-indigo-700 px-6 py-4 mb-3 md:mb-6 rounded-lg shadow">
+    <div class="  h-screen bg-black  ">
+      <div class=" px-5  pt-6 pb-6 border-b">
+        <!-- <div ><i  class="fa fa-angle-left fa-solid fa-2x py-1 text-white"></i></div> -->
+        <ui-icon size="lg" icon="chevronLeft" class=" py-8 w-8 dark:text-white"></ui-icon>
+      </div>
+
+      <div>
+        <a href=""
+          ><img src="/assets/mobile-ui/assets/images/display-pic.png" alt="" id="" class="w-full border-0 "
+        /></a>
+      </div>
+
+      <div class=" flex  mt-14 justify-between items-center gap-4  px-5">
+        <div>
+          <a href=""
+            ><img src="/assets/mobile-ui/assets/images/cam-pic.png" alt="" id="" class="w-full border-0 mt-10"
+          /></a>
+        </div>
+        <div>
+          <a href=""
+            ><img src="/assets/mobile-ui/assets/images/circle-btn.png" alt="" id="" class="w-full border-0  "
+          /></a>
+        </div>
+        <div>
+          <a href=""
+            ><img src="/assets/mobile-ui/assets/images/cam.png" alt="" id="" class="w-full border-0 mt-10"
+          /></a>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div class="dark:bg-gray-800 border dark:border-indigo-700 px-6 py-4 mb-3 md:mb-6 rounded-lg shadow">
       <div>
         <code>ui-mobile-camera</code>
       </div>
-    </div>
+    </div> -->
   `,
 })
 export class WebUiMobileCameraComponent {}
