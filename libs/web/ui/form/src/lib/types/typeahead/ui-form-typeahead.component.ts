c0aeeaeebb8b066804f50b93ad831338f2045cba
nomikhokher <nomankhokher786@gmail.com>
2022-02-26 00:03:58 +0500

    { ui-form-typeahead | dev-select-store }

--- libs/web/ui/form/src/lib/types/typeahead/ui-form-typeahead.component.ts
+++ libs/web/ui/form/src/lib/types/typeahead/ui-form-typeahead.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core'
+import { Component, ChangeDetectionStrategy, OnInit, Input } from '@angular/core'
 import { FormControl } from '@angular/forms'
 import { FieldType } from '@ngx-formly/core'
 import { Subject, Observable } from 'rxjs'
@@ -10,23 +10,77 @@ interface Item {
 
 @Component({
   template: `
-    <ng-select
-      class="custom"
-      [items]="to?.items | async"
-      [placeholder]="to.placeholder"
-      [bindValue]="'id'"
-      [bindLabel]="'name'"
-      [multiple]="to.multiple"
-      [virtualScroll]="true"
-      [formControl]="formControl"
-      [groupBy]="to.groupBy"
-      (change)="onChange($event)"
-      (search)="to?.onSearch($event)"
-    ></ng-select>
+    <div>
+      <label for="combobox" class="block text-sm font-medium text-gray-700 dark:text-white">Assigned to</label>
+      <div class="relative mt-1 w-56 ">
+        <input
+          id="combobox"
+          type="text"
+          value="{{ this.textselect }}"
+          class="rounded-md border border-gray-300 bg-white py-2 pl-3 pr-12 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
+        />
+        <button
+          (click)="onshow()"
+          type="button"
+          class="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none"
+        >
+          <svg
+            class="h-5 w-5 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+
+        <ul
+          *ngIf="listshow === true"
+          class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+        >
+          <li
+            *ngFor="let item of to?.options"
+            class="relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900"
+            (click)="getValue(item)"
+            id="this.itemid"
+          >
+            <div class="flex items-center">
+              <img
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+                class="h-6 w-6 flex-shrink-0 rounded-full"
+              />
+              <span class="ml-3 truncate">{{ item.name }}</span>
+            </div>
+            <span *ngIf="item.tick == true" class="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600">
+              <svg
+                class="h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </li>
+        </ul>
+      </div>
+      <br /><br /><br /><br />
+    </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UiFormTypeaheadComponent extends FieldType {
+export class UiFormTypeaheadComponent extends FieldType implements OnInit {
   formControl!: FormControl
   search$ = new Subject<string>()
   static = false
@@ -35,7 +89,15 @@ export class UiFormTypeaheadComponent extends FieldType {
   options$: Observable<string[]>
   public value: any
 
+  @Input() cboxDetail: Contact
+  listshow?: boolean
+  selected?: any
+  hero?: string
+  textselect?: string
+
   onChange(selection) {
+    console.log('ljhkjh')
+
     if (this.to.onChange) {
       return this.to.onChange(selection)
     }
@@ -43,7 +105,11 @@ export class UiFormTypeaheadComponent extends FieldType {
   }
 
   onSearch({ term }) {
-    console.log('searching', this.formState, this.options$, term)
+    // console.log('searching', this.formState, this.options$, term)
+    // console.log(this.search$);
+    // console.log(this.options$);
+    // console.log(this.selectedItem);
+    // console.log(this.items);
   }
 
   inputMapFn(e: any) {
@@ -62,4 +128,43 @@ export class UiFormTypeaheadComponent extends FieldType {
     this.formControl.setValue(e)
     this.value = e
   }
+  ngOnInit() {
+    this.listshow = false
+    console.log(this.to?.options)
+  }
+
+  public onOptionsSelected(event) {
+    const value = event.target.value
+    this.selected = value
+    console.log(value)
+  }
+  public onshow() {
+    if (this.listshow == true) {
+      this.listshow = false
+    } else {
+      this.listshow = true
+    }
+  }
+  getValue = (item: string) => {
+    this.listshow = false
+    for (let i = 0; i < Object.keys(this.cboxDetail).length; i++) {
+      if (item == this.cboxDetail[i]) {
+        this.cboxDetail[i].tick = true
+        this.hero = this.cboxDetail[i].name
+        this.textselect = this.cboxDetail[i].name
+      } else {
+        this.cboxDetail[i].tick = false
+      }
+    }
+  }
+
+  onSelect(hero): void {
+    this.cboxDetail.name = hero
+  }
+}
+interface Contact {
+  id: string
+  name: string
+  image?: string
+  tick?: boolean
 }
