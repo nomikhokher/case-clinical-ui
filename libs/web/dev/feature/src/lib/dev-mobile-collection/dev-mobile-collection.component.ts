51644723b8e858687d02af114c9e06015471993b
nomikhokher <nomankhokher786@gmail.com>
2022-03-10 19:47:03 +0500

    {dev-mobile-collection) | Component Props

--- libs/web/dev/feature/src/lib/dev-mobile-collection/dev-mobile-collection.component.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-collection/dev-mobile-collection.component.ts
@@ -4,9 +4,18 @@ import { DevMobileCollectionStore } from './dev-mobile-collection.store'
 @Component({
   template: `
     <ng-container *ngIf="vm$ | async as vm">
-      <ui-preview>
-        <!-- INSERT YOUR UI-COMPONENT HERE  -->
-      </ui-preview>
+      <ui-mobile-preview
+        [title]="vm.headerTitle"
+        [githubURL]="vm.githubURL"
+        [directory]="vm.directory"
+        [breadcrumbs]="vm.breadcrumbs"
+        [component_props]="[vm.componentProps]"
+        [component_inputs]="vm.component_inputs"
+        [code]="codePreview[0]"
+        [codeObj]="vm.items"
+      >
+        <ui-mobile-collection></ui-mobile-collection>
+      </ui-mobile-preview>
     </ng-container>
   `,
   providers: [DevMobileCollectionStore],
@@ -14,4 +23,15 @@ import { DevMobileCollectionStore } from './dev-mobile-collection.store'
 export class DevMobileCollectionComponent {
   readonly vm$ = this.store.vm$
   constructor(private readonly store: DevMobileCollectionStore) {}
+  public codePreview: Array<any>
+  ngOnInit(): void {
+    this.vm$.subscribe((result) => {
+      this.codePreview = [
+        `\nimport { WebUiMobileCollectionModule } from '@schema-driven/web/ui/mobile-collection' \n
+<ui-mobile-collection></ui-mobile-collection>
+         \n\n
+        `,
+      ]
+    })
+  }
 }
--- libs/web/dev/feature/src/lib/dev-mobile-collection/dev-mobile-collection.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-collection/dev-mobile-collection.module.ts
@@ -2,14 +2,16 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileCollectionComponent } from './dev-mobile-collection.component'
-import { WebUiPreviewModule } from '@schema-driven/web/ui/preview'
+import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
+//import { WebUiMobileCollectionModule } from '@schema-driven/web/ui/mobile-collection'
 
 @NgModule({
   declarations: [DevMobileCollectionComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileCollectionComponent }]),
-    WebUiPreviewModule,
+    WebUiMobilePreviewModule,
+    // WebUiMobileCollectionModule,
   ],
 })
 export class DevMobileCollectionModule {}
