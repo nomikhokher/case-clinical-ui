8aced84eb0f42596015e7faed2c3215b062686f1
nomikhokher <nomankhokher786@gmail.com>
2022-03-10 17:16:34 +0500

    {dev-mobile-collection} | component routes

--- libs/web/dev/feature/src/lib/web-dev-route.module.ts
+++ libs/web/dev/feature/src/lib/web-dev-route.module.ts
@@ -543,18 +543,9 @@ export const routes: Routes = [
     loadChildren: () =>
       import('./dev-mobile-discovery/dev-mobile-discovery.module').then((m) => m.DevMobileDiscoveryModule),
   },
-  // {
-  //   path: 'mobile-discovery-like',
-  //   loadChildren: () =>
-  //     import('./dev-mobile-discovery-like/dev-mobile-discovery-like.module').then(
-  //       (m) => m.DevMobileDiscoveryLikeModule,
-  //     ),
-  // },
-  // {
-  //   path: 'mobile-discovery-action',
-  //   loadChildren: () =>
-  //     import('./dev-mobile-discovery-action/dev-mobile-discovery-action.module').then(
-  //       (m) => m.DevMobileDiscoveryActionModule,
-  //     ),
-  // },
+  {
+    path: 'mobile-collection',
+    loadChildren: () =>
+      import('./dev-mobile-collection/dev-mobile-collection.module').then((m) => m.DevMobileCollectionModule),
+  },
 ]
