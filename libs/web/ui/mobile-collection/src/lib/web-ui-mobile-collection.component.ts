f8554f12dc81ee1c4ef0ed1b955daf17d7a4b0c8
nomikhokher <nomankhokher786@gmail.com>
2022-03-11 00:17:43 +0500

    {dev-mobile-collection} | UI Component design

--- libs/web/ui/mobile-collection/src/lib/web-ui-mobile-collection.component.ts
+++ libs/web/ui/mobile-collection/src/lib/web-ui-mobile-collection.component.ts
@@ -3,12 +3,39 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'ui-mobile-collection',
   template: `
-    <div class="mx-auto mt-2  w-mobile h-mobile">
-      <div class="bg-white pt-8">
+    <style>
+      /* width */
+      .scroll-section::-webkit-scrollbar {
+        width: 0px;
+      }
+      .scroll-section {
+        height: 633px;
+        overflow-y: scroll;
+        scrollbar-color: white white;
+        scrollbar-width: none;
+      }
+      /* Track */
+      .scroll-section::-webkit-scrollbar-track {
+        background: #fff;
+      }
+
+      /* Handle */
+      .scroll-section::-webkit-scrollbar-thumb {
+        background: #fff;
+      }
+
+      /* Handle on hover */
+      .scroll-section::-webkit-scrollbar-thumb:hover {
+        background: #fff;
+      }
+    </style>
+
+    <div class="mx-auto bg-gray-200 mt-8">
+      <div class="bg-white dark:bg-gray-600">
         <div class="flex px-5 justify-between items-center gap-4 border-b">
-          <div><i class="fa fa-angle-left fa-solid fa-2x py-1"></i></div>
+          <ui-icon size="lg" icon="chevronLeft" class=" py-8 w-8 dark:text-white"></ui-icon>
           <div>
-            <h2 class="text-2xl font-medium title-font pl-1 text-gray-900">Choose Collection</h2>
+            <h2 class="text-2xl font-medium title-font pl-1 dark:text-white">Choose Collection</h2>
           </div>
           <a href="#">
             <svg
@@ -23,39 +50,40 @@ import { Component } from '@angular/core'
             </svg>
           </a>
         </div>
-
-        <section class=" body-font">
-          <div class=" px-5 pt-4 pb-5 mx-auto shadow-lg shadow-blue-500/20 rounded-lg bg-white">
-            <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-              <div class=" mb-10 px-4">
-                <div class="rounded-lg h-64 overflow-hidden">
-                  <img
-                    alt="content"
-                    class="object-cover object-center h-full w-full"
-                    src="/assets/mobile-ui/assets/images/img1.png"
-                  />
+        <div class="scroll-section">
+          <section class=" body-font">
+            <div class=" px-5 pt-4 pb-5 mx-auto shadow-lg shadow-blue-500/20 rounded-lg dark:text-white">
+              <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+                <div class=" mb-10 px-4">
+                  <div class="rounded-lg h-64 overflow-hidden">
+                    <img
+                      alt="content"
+                      class="object-cover object-center h-full w-full"
+                      src="/assets/mobile-ui/assets/images/img1.png"
+                    />
+                  </div>
+                  <h2 class="title-font text-base font-bold font-medium dark:text-white mt-2 ">The Cubes of Destiny</h2>
+                  <p class="leading-relaxed dark:text-blue-300">ERC-1155</p>
                 </div>
-                <h2 class="title-font text-base font-bold font-medium text-gray-900 mt-2 ">The Cubes of Destiny</h2>
-                <p class="leading-relaxed text-base">ERC-1155</p>
               </div>
             </div>
-          </div>
-          <div class=" px-5 pt-4 mt-5 pb-3 mx-auto shadow-lg shadow-blue-500/20 rounded-lg rounded-lg ">
-            <div class="flex flex-wrap -mx-4 -mb-10 text-center">
-              <div class=" mb-10 px-4">
-                <div class="rounded-lg h-64 overflow-hidden">
-                  <img
-                    alt="content"
-                    class="object-cover object-center h-full w-full"
-                    src="/assets/mobile-ui/assets/images/img2.png"
-                  />
+            <div class=" px-5 pt-4 mt-5 pb-3 mx-auto shadow-lg shadow-blue-500/20 rounded-lg rounded-lg ">
+              <div class="flex flex-wrap -mx-4 -mb-10 text-center">
+                <div class=" mb-10 px-4">
+                  <div class="rounded-lg h-64 overflow-hidden">
+                    <img
+                      alt="content"
+                      class="object-cover object-center h-full w-full"
+                      src="/assets/mobile-ui/assets/images/img2.png"
+                    />
+                  </div>
+                  <h2 class="title-font text-base font-bold font-medium dark:text-white mt-2 ">The Cubes of Destiny</h2>
+                  <p class="leading-relaxed dark:text-blue-300">ERC-1155</p>
                 </div>
-                <h2 class="title-font text-base font-bold font-medium text-gray-900 mt-2 ">The Cubes of Destiny</h2>
-                <p class="leading-relaxed text-base">ERC-1155</p>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
     </div>
   `,
