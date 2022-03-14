ef5786026f9e73fafcd1c6182bf31c380bb97594
nomikhokher <nomankhokher786@gmail.com>
2022-03-14 19:09:41 +0500

    {dev-mobile-profile-following} | binding props

--- libs/web/dev/feature/src/lib/dev-mobile-profile-following/dev-mobile-profile-following.component.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-profile-following/dev-mobile-profile-following.component.ts
@@ -4,9 +4,18 @@ import { DevMobileProfileFollowingStore } from './dev-mobile-profile-following.s
 @Component({
   template: `
     <ng-container *ngIf="vm$ | async as vm">
-      <ui-preview>
-        <!-- INSERT YOUR UI-COMPONENT HERE  -->
-      </ui-preview>
+      <ui-mobile-preview
+        [title]="vm.headerTitle"
+        [githubURL]="vm.githubURL"
+        [directory]="vm.directory"
+        [breadcrumbs]="vm.breadcrumbs"
+        [component_props]="[vm.componentProps]"
+        [component_inputs]="vm.component_inputs"
+        [code]="codePreview[0]"
+        [codeObj]="vm.items"
+      >
+        <ui-mobile-profile-following></ui-mobile-profile-following>
+      </ui-mobile-preview>
     </ng-container>
   `,
   providers: [DevMobileProfileFollowingStore],
@@ -14,4 +23,15 @@ import { DevMobileProfileFollowingStore } from './dev-mobile-profile-following.s
 export class DevMobileProfileFollowingComponent {
   readonly vm$ = this.store.vm$
   constructor(private readonly store: DevMobileProfileFollowingStore) {}
+  public codePreview: Array<any>
+  ngOnInit(): void {
+    this.vm$.subscribe((result) => {
+      this.codePreview = [
+        `\nimport { WebUiMobileProfileModule } from '@schema-driven/web/ui/mobile-profile-following' \n
+<ui-mobile-profile-following></ui-mobile-profile-following>
+         \n\n
+        `,
+      ]
+    })
+  }
 }
--- libs/web/dev/feature/src/lib/dev-mobile-profile-following/dev-mobile-profile-following.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-profile-following/dev-mobile-profile-following.module.ts
@@ -2,14 +2,16 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileProfileFollowingComponent } from './dev-mobile-profile-following.component'
-import { WebUiPreviewModule } from '@schema-driven/web/ui/preview'
+import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
+//import { WebUiMobileProfileFollowingModule } from '@schema-driven/web/ui/mobile-profile-following'
 
 @NgModule({
   declarations: [DevMobileProfileFollowingComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileProfileFollowingComponent }]),
-    WebUiPreviewModule,
+    WebUiMobilePreviewModule,
+    //  WebUiMobileProfileFollowingModule,
   ],
 })
 export class DevMobileProfileFollowingModule {}
