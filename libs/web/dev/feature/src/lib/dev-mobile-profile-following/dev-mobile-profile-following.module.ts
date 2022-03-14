8ff6e7bc27a46d612b7d6104679232887b913431
nomikhokher <nomankhokher786@gmail.com>
2022-03-14 21:11:10 +0500

    {ui-mobile-profile-following} | UI Component

--- libs/web/dev/feature/src/lib/dev-mobile-profile-following/dev-mobile-profile-following.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-profile-following/dev-mobile-profile-following.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileProfileFollowingComponent } from './dev-mobile-profile-following.component'
 import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
-//import { WebUiMobileProfileFollowingModule } from '@schema-driven/web/ui/mobile-profile-following'
+import { WebUiMobileProfileFollowingModule } from '@schema-driven/web/ui/mobile-profile-following'
 
 @NgModule({
   declarations: [DevMobileProfileFollowingComponent],
@@ -11,7 +11,7 @@ import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileProfileFollowingComponent }]),
     WebUiMobilePreviewModule,
-    //  WebUiMobileProfileFollowingModule,
+    WebUiMobileProfileFollowingModule,
   ],
 })
 export class DevMobileProfileFollowingModule {}
--- libs/web/ui/mobile-profile-following/src/lib/web-ui-mobile-profile-following.module.ts
+++ libs/web/ui/mobile-profile-following/src/lib/web-ui-mobile-profile-following.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-
+import { WebUiIconModule } from '@schema-driven/web/ui/icon'
 import { WebUiMobileProfileFollowingComponent } from './web-ui-mobile-profile-following.component'
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, WebUiIconModule],
   declarations: [WebUiMobileProfileFollowingComponent],
   exports: [WebUiMobileProfileFollowingComponent],
 })
