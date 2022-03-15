ea852123fc7aa5f14f9dbf682cff337613fc7902
nomikhokher <nomankhokher786@gmail.com>
2022-03-15 21:36:40 +0500

    {ui-mobile-nft-create}|icon import | Dev Comp Link

--- libs/web/dev/feature/src/lib/dev-mobile-nft-create/dev-mobile-nft-create.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-nft-create/dev-mobile-nft-create.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileNftCreateComponent } from './dev-mobile-nft-create.component'
 import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
-//import { WebUiMobileNftCreateModule } from '@schema-driven/web/ui/mobile-nft-create'
+import { WebUiMobileNftCreateModule } from '@schema-driven/web/ui/mobile-nft-create'
 @NgModule({
   declarations: [DevMobileNftCreateComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileNftCreateComponent }]),
     WebUiMobilePreviewModule,
-    // WebUiMobileNftCreateModule,
+    WebUiMobileNftCreateModule,
   ],
 })
 export class DevMobileNftCreateModule {}
