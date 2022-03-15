4e150a7cb2ce69ab06b6e9ad8d904a7fbf4eb7ed
nomikhokher <nomankhokher786@gmail.com>
2022-03-15 19:59:42 +0500

    {dev-mobile-nft-create} | component properties.

--- libs/web/dev/feature/src/lib/dev-mobile-nft-create/dev-mobile-nft-create.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-nft-create/dev-mobile-nft-create.module.ts
@@ -2,14 +2,15 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileNftCreateComponent } from './dev-mobile-nft-create.component'
-import { WebUiPreviewModule } from '@schema-driven/web/ui/preview'
-
+import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
+//import { WebUiMobileNftCreateModule } from '@schema-driven/web/ui/mobile-nft-create'
 @NgModule({
   declarations: [DevMobileNftCreateComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileNftCreateComponent }]),
-    WebUiPreviewModule,
+    WebUiMobilePreviewModule,
+    // WebUiMobileNftCreateModule,
   ],
 })
 export class DevMobileNftCreateModule {}
