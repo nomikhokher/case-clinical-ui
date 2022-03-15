041f61aad8e326e5fb9d5bc7c95f08e3b19a02e5
nomikhokher <nomankhokher786@gmail.com>
2022-03-15 21:22:42 +0500

    { dev-mobile-profile-home } | Set Obj

--- libs/web/dev/feature/src/lib/dev-mobile-profile-home/dev-mobile-profile-home.component.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-profile-home/dev-mobile-profile-home.component.ts
@@ -4,9 +4,18 @@ import { DevMobileProfileHomeStore } from './dev-mobile-profile-home.store'
 @Component({
   template: `
     <ng-container *ngIf="vm$ | async as vm">
-      <ui-preview>
+      <ui-mobile-preview
+        [title]="vm.headerTitle"
+        [githubURL]="vm.githubURL"
+        [directory]="vm.directory"
+        [breadcrumbs]="vm.breadcrumbs"
+        [component_props]="[vm.componentProps]"
+        [component_inputs]="vm.component_inputs"
+        [code]="codePreview[0]"
+        [codeObj]="vm.items"
+      >
         <!-- INSERT YOUR UI-COMPONENT HERE  -->
-      </ui-preview>
+      </ui-mobile-preview>
     </ng-container>
   `,
   providers: [DevMobileProfileHomeStore],
@@ -14,4 +23,15 @@ import { DevMobileProfileHomeStore } from './dev-mobile-profile-home.store'
 export class DevMobileProfileHomeComponent {
   readonly vm$ = this.store.vm$
   constructor(private readonly store: DevMobileProfileHomeStore) {}
+  public codePreview: Array<any>
+  ngOnInit(): void {
+    this.vm$.subscribe((result) => {
+      this.codePreview = [
+        `\nimport { WebUiMobileProfileHomeModule } from '@schema-driven/web/ui/mobile-profile-home' \n
+<ui-mobile-profile-home></ui-mobile-profile-home>
+         \n        
+        `,
+      ]
+    })
+  }
 }
--- libs/web/dev/feature/src/lib/dev-mobile-profile-home/dev-mobile-profile-home.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-profile-home/dev-mobile-profile-home.module.ts
@@ -2,14 +2,16 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileProfileHomeComponent } from './dev-mobile-profile-home.component'
-import { WebUiPreviewModule } from '@schema-driven/web/ui/preview'
+import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
+import { WebUiMobileProfileHomeModule } from '@schema-driven/web/ui/mobile-profile-home'
 
 @NgModule({
   declarations: [DevMobileProfileHomeComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileProfileHomeComponent }]),
-    WebUiPreviewModule,
+    WebUiMobilePreviewModule,
+    WebUiMobileProfileHomeModule,
   ],
 })
 export class DevMobileProfileHomeModule {}
