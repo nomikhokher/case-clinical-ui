2142e71ab2b1e069f478b568424488b85b8d5ea1
nomikhokher <nomankhokher786@gmail.com>
2022-03-17 22:58:48 +0500

    {ui-mobile-nft-complete} | ui icon

--- libs/web/ui/mobile-nft-complete/src/lib/web-ui-mobile-nft-complete.module.ts
+++ libs/web/ui/mobile-nft-complete/src/lib/web-ui-mobile-nft-complete.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-
+import { WebUiIconModule } from '@schema-driven/web/ui/icon'
 import { WebUiMobileNftCompleteComponent } from './web-ui-mobile-nft-complete.component'
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, WebUiIconModule],
   declarations: [WebUiMobileNftCompleteComponent],
   exports: [WebUiMobileNftCompleteComponent],
 })
