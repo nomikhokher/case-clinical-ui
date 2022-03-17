80cef204dd4fe430a3c2872a79445afbfcbbe583
nomikhokher <nomankhokher786@gmail.com>
2022-03-17 19:11:42 +0500

    {dev-mobile-nft-complete} | store data | Module

--- libs/web/dev/feature/src/lib/dev-mobile-nft-complete/dev-mobile-nft-complete.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-nft-complete/dev-mobile-nft-complete.module.ts
@@ -2,14 +2,15 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileNftCompleteComponent } from './dev-mobile-nft-complete.component'
-import { WebUiPreviewModule } from '@schema-driven/web/ui/preview'
-
+import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
+//import { WebUiMobileNftCompleteModule } from '@schema-driven/web/ui/mobile-nft-complete'
 @NgModule({
   declarations: [DevMobileNftCompleteComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileNftCompleteComponent }]),
-    WebUiPreviewModule,
+    WebUiMobilePreviewModule,
+    // WebUiMobileNftCompleteModule,
   ],
 })
 export class DevMobileNftCompleteModule {}
