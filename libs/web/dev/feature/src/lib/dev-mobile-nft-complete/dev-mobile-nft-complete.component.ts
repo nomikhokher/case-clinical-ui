848c67eceae8df70affc20ba687db726811c8602
nomikhokher <nomankhokher786@gmail.com>
2022-03-17 19:56:55 +0500

    {dev-mobile-nft-complete} | component properties

--- libs/web/dev/feature/src/lib/dev-mobile-nft-complete/dev-mobile-nft-complete.component.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-nft-complete/dev-mobile-nft-complete.component.ts
@@ -4,9 +4,18 @@ import { DevMobileNftCompleteStore } from './dev-mobile-nft-complete.store'
 @Component({
   template: `
     <ng-container *ngIf="vm$ | async as vm">
-      <ui-preview>
-        <!-- INSERT YOUR UI-COMPONENT HERE  -->
-      </ui-preview>
+      <ui-mobile-preview
+        [title]="vm.headerTitle"
+        [githubURL]="vm.githubURL"
+        [directory]="vm.directory"
+        [breadcrumbs]="vm.breadcrumbs"
+        [component_props]="[vm.componentProps]"
+        [component_inputs]="vm.component_inputs"
+        [code]="codePreview[0]"
+        [codeObj]="vm.items"
+      >
+        <ui-mobile-nft-complete></ui-mobile-nft-complete>
+      </ui-mobile-preview>
     </ng-container>
   `,
   providers: [DevMobileNftCompleteStore],
@@ -14,4 +23,15 @@ import { DevMobileNftCompleteStore } from './dev-mobile-nft-complete.store'
 export class DevMobileNftCompleteComponent {
   readonly vm$ = this.store.vm$
   constructor(private readonly store: DevMobileNftCompleteStore) {}
+  public codePreview: Array<any>
+  ngOnInit(): void {
+    this.vm$.subscribe((result) => {
+      this.codePreview = [
+        `\nimport { WebUiMobileNftCompleteModule } from '@schema-driven/web/ui/mobile-nft-complete' \n
+<ui-mobile-nft-complete></ui-mobile-nft-complete>
+         \n\n
+        `,
+      ]
+    })
+  }
 }
