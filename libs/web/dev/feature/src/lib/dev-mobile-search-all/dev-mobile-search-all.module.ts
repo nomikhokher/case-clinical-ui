ba3784a7678724aa2d33cd2cb395e25f77528a43
nomikhokher <nomankhokher786@gmail.com>
2022-03-18 22:22:00 +0500

    {dev-mobile-search-all}| Components Linking | Icon

--- libs/web/dev/feature/src/lib/dev-mobile-search-all/dev-mobile-search-all.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-search-all/dev-mobile-search-all.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileSearchAllComponent } from './dev-mobile-search-all.component'
 import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
-//import { WebUiMobileSearchAllModule } from '@schema-driven/web/ui/mobile-search-all'
+import { WebUiMobileSearchAllModule } from '@schema-driven/web/ui/mobile-search-all'
 @NgModule({
   declarations: [DevMobileSearchAllComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileSearchAllComponent }]),
     WebUiMobilePreviewModule,
-    //  WebUiMobileSearchAllModule,
+    WebUiMobileSearchAllModule,
   ],
 })
 export class DevMobileSearchAllModule {}
