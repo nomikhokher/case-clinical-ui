6c20fe7b292ce6d971c786817caffbb75467f890
nomikhokher <nomankhokher786@gmail.com>
2022-03-21 22:57:22 +0500

    {dev-mobile-search-creator} dev&UI component Link

--- libs/web/dev/feature/src/lib/dev-mobile-search-creator/dev-mobile-search-creator.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-search-creator/dev-mobile-search-creator.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileSearchCreatorComponent } from './dev-mobile-search-creator.component'
 import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
-//import { WebUiMobileSearchCreatorModule } from '@schema-driven/web/ui/mobile-search-creator'
+import { WebUiMobileSearchCreatorModule } from '@schema-driven/web/ui/mobile-search-creator'
 @NgModule({
   declarations: [DevMobileSearchCreatorComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileSearchCreatorComponent }]),
     WebUiMobilePreviewModule,
-    //WebUiMobileSearchCreatorModule,
+    WebUiMobileSearchCreatorModule,
   ],
 })
 export class DevMobileSearchCreatorModule {}
