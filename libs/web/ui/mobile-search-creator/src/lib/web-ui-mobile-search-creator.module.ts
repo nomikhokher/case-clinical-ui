61f4ca4affe8607b5a35fee1e03c827984bdce30
nomikhokher <nomankhokher786@gmail.com>
2022-03-21 23:00:10 +0500

    {ui-mobile-search-creator} | ui icons binding

--- libs/web/ui/mobile-search-creator/src/lib/web-ui-mobile-search-creator.module.ts
+++ libs/web/ui/mobile-search-creator/src/lib/web-ui-mobile-search-creator.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-
+import { WebUiIconModule } from '@schema-driven/web/ui/icon'
 import { WebUiMobileSearchCreatorComponent } from './web-ui-mobile-search-creator.component'
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, WebUiIconModule],
   declarations: [WebUiMobileSearchCreatorComponent],
   exports: [WebUiMobileSearchCreatorComponent],
 })
