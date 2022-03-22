02b001b4a89c699c943e8e3c7d5ccd1a36b24b8f
nomikhokher <nomankhokher786@gmail.com>
2022-03-23 00:22:27 +0500

    {dev-mobile-search-artwork} | final component

--- libs/web/ui/mobile-search-artwork/src/lib/web-ui-mobile-search-artwork.component.ts
+++ libs/web/ui/mobile-search-artwork/src/lib/web-ui-mobile-search-artwork.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core'
     <style>
       /* width */
       .scroll-section-vertical {
-        height: 299px;
+        height: 604px;
       }
       .scroll-section-horizontal {
         /* height: 203px; */
@@ -102,71 +102,148 @@ import { Component } from '@angular/core'
             </div>
           </div>
         </section>
-
-        <div class="mt-6">
-          <div class="">
-            <img
-              src="/assets/mobile-ui/assets/images/search-art-img.png"
-              alt=""
-              class="h-72 w-full object-cover rounded-tr-xl rounded-tl-xl"
-            />
-          </div>
-          <div class="shadow-blue-100 shadow-xl  rounded-br-xl rounded-bl-xl">
-            <div class="shadow-blue-100 shadow-xl px-8 py-4 ">
-              <div class="flex justify-between items-center">
-                <div class="bg-gray-100 py-1.5 px-4 rounded-3xl text-black font-medium">
-                  <h2>⏳ 1h 28m 11s</h2>
-                </div>
-                <div class="flex items-center gap-4 text-black">
-                  <a href=""
-                    ><img src="/assets/mobile-ui/assets/images/BlackHeart.png" alt="" id="" class="w-full h-full"
-                  /></a>
-                  <a href="">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      class="h-6 w-6"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
-                      />
-                    </svg>
-                  </a>
-                </div>
-              </div>
-              <h2 class="text-3xl font-bold text-black pt-4">Sugar Rush</h2>
+        <section class="scroll-section scroll-section-vertical ">
+          <div class=" justify-between px-4 pt-4 items-center">
+            <div class="">
+              <img
+                src="/assets/mobile-ui/assets/images/search-art-img.png"
+                alt=""
+                class="h-72 w-full object-cover rounded-tr-xl rounded-tl-xl"
+              />
             </div>
-            <div class="px-8 pt-4 pb-6 flex justify-between items-center">
-              <div class="flex w-full gap-2 items-center">
-                <div class="">
-                  <img
-                    src="/assets/mobile-ui/assets/images/Avatar.png"
-                    alt=""
-                    class="w-10 h-10 object-cover rounded-full"
-                  />
+            <div
+              class="shadow-blue-100 shadow-xl  rounded-br-xl rounded-bl-xl  shadow-blue-500/20 rounded-lg dark:bg-gray-700"
+            >
+              <div class="shadow-blue-100 shadow-xl px-8 py-4 ">
+                <div class="flex justify-between items-center">
+                  <div class="bg-gray-100 py-1.5 px-4 rounded-3xl text-black font-medium">
+                    <h2>⏳ 1h 28m 11s</h2>
+                  </div>
+                  <div class="flex items-center gap-4 text-black">
+                    <a href=""
+                      ><img src="/assets/mobile-ui/assets/images/BlackHeart.png" alt="" id="" class="w-full h-full"
+                    /></a>
+                    <a href="">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                        />
+                      </svg>
+                    </a>
+                  </div>
                 </div>
-                <div class="text-black">
-                  <small class="text-xs text-gray-400">Creator</small>
-                  <p class="text-base font-bold">Kevin</p>
-                </div>
+                <h2 class="text-3xl font-bold dark:text-white  pt-4">Sugar Rush</h2>
               </div>
-              <div class="flex w-full gap-2 items-center justify-end">
-                <div class="py-2 px-3 bg-gray-100 rounded-full">
-                  <img src="/assets/mobile-ui/assets/images/ethereumCurr.png" alt="" class="w-full h-7 rounded-full" />
+              <div class="px-8 pt-4 pb-6 flex justify-between items-center">
+                <div class="flex w-full gap-2 items-center">
+                  <div class="">
+                    <img
+                      src="/assets/mobile-ui/assets/images/Avatar.png"
+                      alt=""
+                      class="w-10 h-10 object-cover rounded-full"
+                    />
+                  </div>
+                  <div class="dark:text-white ">
+                    <small class="text-xs text-gray-400">Creator</small>
+                    <p class="text-base font-bold dark:text-white ">Kevin</p>
+                  </div>
                 </div>
-                <div class="text-black">
-                  <small class="text-xs text-gray-400">Reserve price</small>
-                  <p class="text-base font-bold">0.32 ETH</p>
+                <div class="flex w-full gap-2 items-center justify-end">
+                  <div class="py-2 px-3 bg-gray-100 rounded-full">
+                    <img
+                      src="/assets/mobile-ui/assets/images/ethereumCurr.png"
+                      alt=""
+                      class="w-full h-7 rounded-full"
+                    />
+                  </div>
+                  <div class="dark:text-white ">
+                    <small class="text-xs text-gray-400">Reserve price</small>
+                    <p class="text-base font-bold dark:text-white ">0.32 ETH</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+          <div class=" justify-between px-4 pt-4 items-center">
+            <div class="">
+              <img
+                src="/assets/mobile-ui/assets/images/home-scroll-img.png"
+                alt=""
+                class="h-72 w-full object-cover rounded-tr-xl rounded-tl-xl"
+              />
+            </div>
+            <div
+              class="shadow-blue-100 shadow-xl  rounded-br-xl rounded-bl-xl  shadow-blue-500/20 rounded-lg dark:bg-gray-700"
+            >
+              <div class="shadow-blue-100 shadow-xl px-8 py-4 ">
+                <div class="flex justify-between items-center">
+                  <div class="bg-gray-100 py-1.5 px-4 rounded-3xl text-black font-medium">
+                    <h2>⏳ 1h 28m 11s</h2>
+                  </div>
+                  <div class="flex items-center gap-4 text-black">
+                    <a href=""
+                      ><img src="/assets/mobile-ui/assets/images/BlackHeart.png" alt="" id="" class="w-full h-full"
+                    /></a>
+                    <a href="">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        class="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+                        />
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+                <h2 class="text-3xl font-bold dark:text-white  pt-4">Sugar Rush</h2>
+              </div>
+              <div class="px-8 pt-4 pb-6 flex justify-between items-center">
+                <div class="flex w-full gap-2 items-center">
+                  <div class="">
+                    <img
+                      src="/assets/mobile-ui/assets/images/Avatar.png"
+                      alt=""
+                      class="w-10 h-10 object-cover rounded-full"
+                    />
+                  </div>
+                  <div class="dark:text-white ">
+                    <small class="text-xs text-gray-400">Creator</small>
+                    <p class="text-base font-bold dark:text-white ">Kevin</p>
+                  </div>
+                </div>
+                <div class="flex w-full gap-2 items-center justify-end">
+                  <div class="py-2 px-3 bg-gray-100 rounded-full">
+                    <img
+                      src="/assets/mobile-ui/assets/images/ethereumCurr.png"
+                      alt=""
+                      class="w-full h-7 rounded-full"
+                    />
+                  </div>
+                  <div class="dark:text-white ">
+                    <small class="text-xs text-gray-400">Reserve price</small>
+                    <p class="text-base font-bold dark:text-white ">0.32 ETH</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   `,
