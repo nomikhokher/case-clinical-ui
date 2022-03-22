daff93889df2c95d11e391a13273ebc50a87d53d
nomikhokher <nomankhokher786@gmail.com>
2022-03-22 21:12:50 +0500

    {dev-mobile-search-artwork} | Comp props | UI Link

--- libs/web/dev/feature/src/lib/dev-mobile-search-artwork/dev-mobile-search-artwork.component.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-search-artwork/dev-mobile-search-artwork.component.ts
@@ -4,9 +4,18 @@ import { DevMobileSearchArtworkStore } from './dev-mobile-search-artwork.store'
 @Component({
   template: `
     <ng-container *ngIf="vm$ | async as vm">
-      <ui-preview>
-        <!-- INSERT YOUR UI-COMPONENT HERE  -->
-      </ui-preview>
+      <ui-mobile-preview
+        [title]="vm.headerTitle"
+        [githubURL]="vm.githubURL"
+        [directory]="vm.directory"
+        [breadcrumbs]="vm.breadcrumbs"
+        [component_props]="vm.componentProps"
+        [component_inputs]="vm.component_inputs"
+        [code]="codePreview[0]"
+        [codeObj]="vm.items"
+      >
+        <ui-mobile-search-artwork></ui-mobile-search-artwork>
+      </ui-mobile-preview>
     </ng-container>
   `,
   providers: [DevMobileSearchArtworkStore],
@@ -14,4 +23,15 @@ import { DevMobileSearchArtworkStore } from './dev-mobile-search-artwork.store'
 export class DevMobileSearchArtworkComponent {
   readonly vm$ = this.store.vm$
   constructor(private readonly store: DevMobileSearchArtworkStore) {}
+  public codePreview: Array<any>
+  ngOnInit(): void {
+    this.vm$.subscribe((result) => {
+      this.codePreview = [
+        `\nimport { WebUiMobileSearchArtworkModule } from \n'@schema-driven/web/ui/mobile-search-artwork' \n
+<ui-mobile-search-artwork></ui-mobile-search-artwork>
+         \n\n
+        `,
+      ]
+    })
+  }
 }
--- libs/web/dev/feature/src/lib/dev-mobile-search-artwork/dev-mobile-search-artwork.module.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-search-artwork/dev-mobile-search-artwork.module.ts
@@ -2,14 +2,15 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { DevMobileSearchArtworkComponent } from './dev-mobile-search-artwork.component'
-import { WebUiPreviewModule } from '@schema-driven/web/ui/preview'
-
+import { WebUiMobilePreviewModule } from '@schema-driven/web/ui/mobile-preview'
+//import { WebUiMobileSearchArtworkModule } from '@schema-driven/web/ui/mobile-search-artwork'
 @NgModule({
   declarations: [DevMobileSearchArtworkComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: DevMobileSearchArtworkComponent }]),
-    WebUiPreviewModule,
+    WebUiMobilePreviewModule,
+    // WebUiMobileSearchArtworkModule,
   ],
 })
 export class DevMobileSearchArtworkModule {}
