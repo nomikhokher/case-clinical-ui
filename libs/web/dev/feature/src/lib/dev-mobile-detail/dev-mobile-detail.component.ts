f2fbee94c11b5348c9ad412950865c6edfa76e87
nomikhokher <nomankhokher786@gmail.com>
2022-03-24 17:21:10 +0500

    {dev-mobile-detail} | dev component  props

--- libs/web/dev/feature/src/lib/dev-mobile-detail/dev-mobile-detail.component.ts
+++ libs/web/dev/feature/src/lib/dev-mobile-detail/dev-mobile-detail.component.ts
@@ -4,9 +4,18 @@ import { DevMobileDetailStore } from './dev-mobile-detail.store'
 @Component({
   template: `
     <ng-container *ngIf="vm$ | async as vm">
-      <ui-preview>
-        <!-- INSERT YOUR UI-COMPONENT HERE  -->
-      </ui-preview>
+      <ui-mobile-preview
+        [title]="vm.headerTitle"
+        [githubURL]="vm.githubURL"
+        [directory]="vm.directory"
+        [breadcrumbs]="vm.breadcrumbs"
+        [component_props]="[vm.componentProps]"
+        [component_inputs]="vm.component_inputs"
+        [code]="codePreview[0]"
+        [codeObj]="vm.items"
+      >
+        <ui-mobile-detail></ui-mobile-detail>
+      </ui-mobile-preview>
     </ng-container>
   `,
   providers: [DevMobileDetailStore],
@@ -14,4 +23,15 @@ import { DevMobileDetailStore } from './dev-mobile-detail.store'
 export class DevMobileDetailComponent {
   readonly vm$ = this.store.vm$
   constructor(private readonly store: DevMobileDetailStore) {}
+  public codePreview: Array<any>
+  ngOnInit(): void {
+    this.vm$.subscribe((result) => {
+      this.codePreview = [
+        `\nimport { WebUiMobileDetailModule } from '@schema-driven/web/ui/mobile-detail' \n
+<ui-mobile-detail></ui-mobile-detail>
+         \n\n
+        `,
+      ]
+    })
+  }
 }
